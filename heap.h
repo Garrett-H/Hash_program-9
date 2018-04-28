7bee4a6735f1c837a7ef95924c1bfa6965189313
Ianmnevi <ianmnevi@ut.utm.edu>
2018-04-27 19:45:23 -0500

    Insert Working

--- heap.h
+++ heap.h
@@ -10,12 +10,14 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 template <typename T>
 class heap
 {
  public:
   heap();
-  ~heap();
+  //~heap();
   int size();
   void insert(T value);
   void remove_max();
@@ -27,4 +29,114 @@ class heap
   T root;
 };
 
-#endif // HEAP_H
+template <typename T>
+heap<T>::heap() {
+  //root = buffer[1];
+}
+/*
+template <typename T>
+heap<T>::~heap() {
+  //root = buffer[1];
+
+}
+*/
+template <typename T>
+int heap<T>::size() {
+  return buffer.size();
+}
+
+template <typename T>
+void heap<T>::insert(T value) {
+  if(buffer.size() == 0){
+    buffer.push_back(value);
+
+  }
+    //buffer.push_back(value);
+  //add element to the end of the heap
+
+  else{
+    buffer.push_back(value);
+    int childLocation  = buffer.size();
+    int parentLocation = childLocation/2;
+    //if the parent is 1
+    if(parentLocation == 0)
+    parentLocation++;
+    //if parent is less than: Swap
+    while(buffer[parentLocation] < buffer[childLocation]) {
+        T temp = buffer[parentLocation];
+        buffer[parentLocation] = buffer[childLocation];
+        buffer[childLocation] = temp;
+        //changing for swap
+        childLocation = parentLocation;
+        parentLocation /= 2;
+        if(parentLocation == 0)
+        parentLocation++;
+    }
+
+
+  }
+
+}
+
+template <typename T>
+void heap<T>::remove_max() {
+  int i=1;
+  buffer[i]=buffer.back();
+  buffer.erase(buffer.end());
+  //loops until buffer is semi-sorted again
+  this->removeHelper(i, (i*2+1), (i*2));
+}
+
+template <typename T>
+void heap<T>::removeHelper(int curr, int left, int right) {
+  //if the current is less than either of it's children
+  if(buffer[curr] < buffer[left] || buffer[curr] < buffer[right]) {
+    //used for temp holding
+    T temp;
+    //false=left or true=right child is changed
+    bool goRight;
+
+    //if left is bigger
+    if(buffer[left] > buffer[right]) {
+      temp = buffer[curr];
+      buffer[curr] = buffer[left];
+      buffer[left] = temp;
+      goRight = false;
+    }
+    //if right is bigger || if right is equal to left
+    else {
+      temp = buffer[curr];
+      buffer[curr] = buffer[right];
+      buffer[right] = temp;
+      goRight = true;
+    }
+
+    //afterword move on to the new location
+    if(goRight) {
+      curr = right;
+      left = right*2;
+      right = left+1;
+     this->removeHelper(curr, left, right);
+    }
+    else {
+      curr = left;
+      right = right*2+1;
+      left = right-1;
+      this->removeHelper(curr, left, right);
+    }
+  }
+  //if current is bigger than either leave it
+}
+
+template <typename T>
+T heap<T>::max() {
+  return buffer[1];
+}
+
+template <typename T>
+void heap<T>::print() {
+  for(int i=1; i != size(); i++)
+    cout << buffer[i] << endl;
+}
+
+#endif //HEAP_H
--- main.cpp
+++ main.cpp
@@ -8,47 +8,72 @@
 #include <cstdlib>
 #include "heap.h"
 
+
 using namespace std;
 
 int main()
 {
     heap<int> userheap;
+    char a;
+    //userheap =  new heap<int>;
     cout<< "Starting heap<int>" << endl;
     cout <<"For Loop to use Insert for series of numbers" << endl;
+    int y = 0;
     cout << endl;
-    for(int x = 0; x < 50 ; x++)
-      userheap.insert(rand()%100);
+    for(int x = 0; x < 50 ; x++){
+
+        y = rand()%100;
+        userheap.insert(y);
+        //userheap.print();
+
+    }
+
+    cin >> a;
 
     cout << "Print Heap" << endl;
     userheap.print();
     cout << endl;
 
+    cin >> a;
+
     cout << "heap size is: " << userheap.size() << endl;
+    cin >> a;
 
     cout << "heap max is: " << userheap.max() << endl;
+    cin >> a;
 
     cout << "Removing Max " << endl;
     userheap.remove_max();
+    cin >> a;
 
     cout << "Next Max " << userheap.max() << endl;
+    cin >> a;
 
     heap<char> charHeap;
     cout<< "Starting heap<char>" << endl;
     cout <<"For Loop to use Insert for series of characters" << endl;
     cout << endl;
-    for(int x = 0; x < 50 ; x++)
-      charHeap.insert(rand()%100 + 32);
-    
+    for(int x = 0; x < 50 ; x++){
+
+        charHeap.insert(rand()%100 + 32);
+    }
+    cin >> a;
+
+
     cout << "Print Heap" << endl;
     charHeap.print();
     cout << endl;
+    cin >> a;
 
     cout << "heap size is: " << charHeap.size() << endl;
+    cin >> a;
 
     cout << "heap max is: " << charHeap.max() << endl;
+    cin >> a;
 
     cout << "Removing Max " << endl;
     charHeap.remove_max();
+    cin >> a;
 
     cout << "Next Max " << charHeap.max();
 
